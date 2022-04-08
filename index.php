de5c5a0c4e18aff495fdc61d678dc74fff65a336
Dawid Szewczyk <dawid.szewczyk.pk@protonmail.com>
2022-04-08 10:15:34 +0200

    register

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo 'Hi there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('projects', 'ProjectController');
+Router::post('login', 'SecurityController');
+Router::post('addProject', 'ProjectController');
+Router::post('register', 'SecurityController');
+Router::post('search', 'ProjectController');
+Router::get('like', 'ProjectController');
+Router::get('dislike', 'ProjectController');
+Router::get('map', 'MapController');
+Router::get('places', 'MapController');
+
+Router::run($path);
